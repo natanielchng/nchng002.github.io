e27db5ca4f2a1acb23fcac92e2ebeaded304cd9e
Nataniel Chng <48916675+nchng002@users.noreply.github.com>
2023-01-02 14:58:36 +0800

    Update website

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 let converter = new showdown.Converter()
 
-$.get("index.md", (response) => {
-    $("#main_content_div").html(converter.makeHtml(response))
+$(document).ready(function () {
+    load_page("pages/home.md")
+    $('#home').click(function () {
+        load_page("pages/home.md")
+    })
+    $('#experiments').click(function () {
+        load_page("pages/experiments.md")
+    })
 })
+
+function load_page(page_file) {
+    $.get(page_file, (response) => {
+        $("#main").html(converter.makeHtml(response))
+    })
+}
